d9538618c3f1610c4829c703bdf02005f0674a23
saludsoporte <salud.aplicaciones@gmail.com>
2021-12-06 09:49:24 -0600

    configuracion nginx

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
-working_directory "/home/soporte/sisolc"
-pid "/home/soporte/sisolc/tmp/pids/unicorn.pid"
-stderr_path "/home/soporte/sisolc/log/unicorn.log"
-stdout_path "/home/soporte/sisolc/log/unicorn.log"
+working_directory "/home/ruby3/sisolc/sisolc"
+pid "/home/ruby3/sisolc/sisolc/tmp/pids/unicorn.pid"
+stderr_path "/home/ruby3/sisolc/sisolc/log/unicorn.log"
+stdout_path "/home/ruby3/sisolc/sisolc/log/unicorn.log"
 
 listen "/tmp/unicorn.sisolc.sock", :backlog=>64
 #listen "0.0.0.0:8443", :tcp_nopush => true
